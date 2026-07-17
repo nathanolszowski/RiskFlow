c4cbd98b34dd6450b9f4f7fb4f03cd196d47399a
nathanolszowski <olszowski.nathan@outlook.fr>
2026-07-17 15:00:59 +0200

    fix/accounts: redirection url problem when disconnecting solved

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
 ]
